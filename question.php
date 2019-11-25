fac45c01d5dfaab99d900e16ee4430ec380497f6
Ito Kanade <kanade.ito01069@gmail.com>
2019-11-25 16:24:14 +0900

    today

--- question.php
+++ question.php
@@ -1,4 +1,5 @@
 <?php
+sleep(3);
 require_once("common.php");
 $i = rand(0,count($question)-1);
 ?>
@@ -14,9 +15,25 @@ $i = rand(0,count($question)-1);
 <body>
     <h1>Question</h1>
     <?= $question[$i][0]?>
+<form action ="result.php">
+    <input type="hidden" name="qid"value="<?php $i ?>">
+    <input type="text"id ="text-answer" name="answer">
+    <button id="btn-answer">kaitou</button>
+</form>
+<script>
+   document.querySelector("#btn-answer").addEventListener("click",[e] =>{
+       let var document.querySelector("#text-answer").value;
+       if(answer == ""){
+       alert("nyuuryokusitekudasai");
+       answer.focus();
+       answer.style.backgroundColor = "Pink";
+       e.prevnentDefault();
+       }else{
 
-<input type="hidden" name="qid"value="<?php $i ?>">
-
+         document.querySelector("#text-answer").innerHTML ="...send "
+       }
+   });
+</script>
     
 </body>
 </html>
